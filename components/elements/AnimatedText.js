284de6875f08e12f900c85a6e9838c8691c4307f
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-20 22:20:05 +0530

    improved text animation

--- components/elements/AnimatedText.js
+++ components/elements/AnimatedText.js
@@ -3,8 +3,14 @@ import TextLoop from "react-text-loop/lib/components/TextLoop";
 
 export default function AnimatedText() {
   return (
-    <div className="text-green-200 text-7xl font-semibold overflow-y-hidden">
-        Create{" "}<TextLoop><span>Beautiful</span><span>Modern</span><span>Interactive</span></TextLoop>{" "}ReadMe
+    <div className="text-zinc-500 text-6xl font-medium overflow-y-hidden">
+      Create{" "}
+      <TextLoop>
+        <span className="text-rose-200 underline underline-offset-4 decoration-dashed">Beautiful</span>
+        <span className="text-cyan-200 underline underline-offset-4 decoration-dashed">Modern</span>
+        <span className="text-amber-200 underline underline-offset-4 decoration-dashed">Interactive</span>
+      </TextLoop>{" "}
+      ReadMe
     </div>
   );
 }
