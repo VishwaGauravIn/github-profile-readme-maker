26c6cee98cd9b71f1015b6a2f96c04ff414c7a0f
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-20 21:36:51 +0530

    hide overflow for animated text

--- components/elements/AnimatedText.js
+++ components/elements/AnimatedText.js
@@ -3,7 +3,7 @@ import TextLoop from "react-text-loop/lib/components/TextLoop";
 
 export default function AnimatedText() {
   return (
-    <div className="text-green-200 text-5xl font-semibold">
+    <div className="text-green-200 text-5xl font-semibold overflow-y-hidden">
         Create{" "}<TextLoop><span>Beautiful</span><span>Modern</span><span>Interactive</span></TextLoop>{" "}ReadMe
     </div>
   );
