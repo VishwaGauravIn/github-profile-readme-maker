f5383a904aa89be8a0841391febabf91f6bffcfa
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-21 18:30:19 +0530

    design layout design

--- components/elements/TextInputWithIcon.js
+++ components/elements/TextInputWithIcon.js
@@ -1,25 +1,29 @@
 import React from "react";
 
-export default function TextInputWithIcon({ id, label, children, placeholder }) {
+export default function TextInputWithIcon({
+  id,
+  children,
+  placeholder,
+}) {
   return (
-    <div className="flex">
+    <div className="flex w-full md:w-5/12 my-2">
       <input
         type="text"
         id={id}
         placeholder={placeholder}
-        className="w-full md:w-4/12 h-16 p-4 border-b-2 bg-transparent outline-none border-green-200 focus:z-10"
+        className="w-full h-16 p-4 border-b-2 bg-transparent outline-none border-green-300 focus:border-green-200 focus:z-10"
       />
 
       <label for={id} className="p-5">
-        <span className="sr-only"> {label} </span>
+        <span className="sr-only"> {id} </span>
 
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
-          viewBox="0 0 16 16"
+          viewBox="-0.5 0 20 16"
           stroke="currentColor"
           aria-hidden="true"
-          className="w-6 h-6 text-green-200"
+          className="w-6 h-6 text-green-300"
         >
           {children}
         </svg>
