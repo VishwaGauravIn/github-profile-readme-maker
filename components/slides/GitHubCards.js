656ed503cb439029961c857a683848af383a6450
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-22 01:25:25 +0530

    added more options in Socials

--- components/slides/GitHubCards.js
+++ components/slides/GitHubCards.js
@@ -20,7 +20,7 @@ export default function GitHubStats() {
             Flex your GitHub Stats
           </p>
           {/* Options */}
-          <div className="flex flex-wrap justify-center">
+          <div className="flex flex-wrap justify-center items-center">
               Theme:
             <select
               id="theme"
