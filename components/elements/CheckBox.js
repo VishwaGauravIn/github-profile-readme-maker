5b23cecf8684a991d22f7fc30454d8ee326898c7
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-22 17:13:13 +0530

    added more options in extras

--- components/elements/CheckBox.js
+++ components/elements/CheckBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function CheckBox({id,title}) {
-  return <p className="flex w-max items-center"><input type="checkbox" name="" id={id} defaultChecked="true" className="w-4 h-4 mr-2 outline-none"/>{title}</p>;
+  return <p className="flex w-max items-center my-2"><input type="checkbox" name="" id={id} defaultChecked="true" className="w-4 h-4 mr-2 outline-none"/>{title}</p>;
 }
--- components/slides/Extras.js
+++ components/slides/Extras.js
@@ -9,8 +9,16 @@ export default function Extras() {
   const [theme, setTheme] = useState("radical");
   const [border, setBorder] = useState(false);
   const [background, setBackground] = useState(false);
+  const [quoteTheme, setQuoteTheme] = useState("radical");
+  const [layout, setLayout] = useState("horizontal")
+  function changeLayout(){
+      if(layout === "horizontal"){
+          setLayout("vetical")
+      }else{
+          setLayout("horizontal")
+      }
+  }
   function onNext() {
-    
     setIsVisible(true);
   }
   return (
@@ -55,8 +63,45 @@ export default function Extras() {
               src={`https://github-profile-trophy.vercel.app/?username=${username}&theme=${theme}&no-frame=${border}&no-bg=${background}&margin-w=4`}
               alt=""
             />
-            <CheckBox id="statschk" title="Add GitHub Trophies"/>
-            
+            <CheckBox id="statschk" title="Add GitHub Trophies" />
+            {/* Options */}
+            <div className="flex flex-wrap justify-center items-center mt-4 my-2">
+              Theme:
+              <select
+                id="quotetheme"
+                onChange={() =>
+                  setQuoteTheme(document.getElementById("quotetheme").value)
+                }
+                className="bg-transparent py-1 px-2 outline-none"
+              >
+                {quoteThemes.map((item) => {
+                  return (
+                    <option key={item} value={item} className="bg-zinc-900">
+                      {item}
+                    </option>
+                  );
+                })}
+              </select>
+              <FilterButton title="Layout" onClick={() => changeLayout()} />
+            </div>
+            <img
+              className="m-2 select-none pointer-events-none"
+              draggable="false"
+              id="stats"
+              src={`https://quotes-github-readme.vercel.app/api?type=${layout}&theme=${quoteTheme}`}
+              alt=""
+            />
+            <CheckBox id="quotechk" title="Add Random Dev Quotes" />
+            {/* Visitors Badge */}
+            <img
+              className="m-2 select-none pointer-events-none"
+              draggable="false"
+              id="stats"
+              src={`https://komarev.com/ghpvc/?username=${username}&label=Visitors+Count&color=brightgreen`}
+              alt=""
+            />
+            <CheckBox id="visitorschk" title="Add Visitors Count" />
+            <CheckBox id="memechk" title="Add Random Memes" />
           </div>
           <NextButton onClick={() => onNext()} />
         </div>
@@ -87,6 +132,15 @@ const themes = [
   "matrix",
   "apprentice",
   "dark_dimmed",
+];
+
+const quoteThemes = [
+  "light",
+  "dark",
+  "radical",
+  "merko",
+  "gruvbox",
+  "tokyonight",
 ];
 
 export var extras = ``;
