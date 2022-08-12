ce916e0bccf9ae3e3d42e75c2cd6ebe449b08552
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-08-12 21:04:54 +0530

    updated font import

--- pages/index.js
+++ pages/index.js
@@ -45,13 +45,6 @@ export default function Home() {
           content="https://gprm.itsvg.in/webimg.png"
         />
         <link rel="icon" href="/logo.png" />
-        {/* Fonts in Head for Fast Serving */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
       {/* Google Analytics */}
       <Script
