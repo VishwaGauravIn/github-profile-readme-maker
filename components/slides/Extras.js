e2ccfbcb3074f16551f21935d744ddd447483693
Vishwa Gaurav <vishwagauravin@gmail.com>
2023-08-12 02:39:21 +0530

    fixed sizing

--- components/slides/Extras.js
+++ components/slides/Extras.js
@@ -99,7 +99,7 @@ export default function Extras({ back }) {
         extras +
         `
 ### 😂 Random Dev Meme
-<img src='https://randommeme-five.vercel.app/' width="512px" height = "400px !important"/>
+<img src='https://randommeme-five.vercel.app/' style="height: 400px;"/>
 `;
     }
     if (document.getElementById("visitorschk").checked === true) {
