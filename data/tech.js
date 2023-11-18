b8a21d732a3761ab6d0233bc41023a445f5afdb9
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-11-18 19:41:40 +0530

    Update tech.js with new badges

--- data/tech.js
+++ data/tech.js
@@ -1065,19 +1065,19 @@ export const data = {
   ],
   others: [
     {
-      label: "GIT",
-      url: "![GIT](https://img.shields.io/badge/Git-fc6d26?style=for-the-badge&logo=git&logoColor=white)",
-    },
-    {
-      label: "LINUX",
-      url: "![LINUX](https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black)",
+      label: "AIRBNB",
+      url: "![Airbnb](https://img.shields.io/badge/Airbnb-%23ff5a5f.svg?style=for-the-badge&logo=Airbnb&logoColor=white)",
     },
     {
       label: "ALFRED",
       url: "![Alfred](https://img.shields.io/badge/alfred-%235C1F87.svg?style=for-the-badge&logo=alfred)",
     },
     {
-      label: "AQUA",
+      label: "ANSIBLE",
+      url: "![Ansible](https://img.shields.io/badge/ansible-%231A1918.svg?style=for-the-badge&logo=ansible&logoColor=white)",
+    },
+    {
+      label: "AQUA SEC",
       url: "![AquaSec](https://img.shields.io/badge/aqua-%231904DA.svg?style=for-the-badge&logo=aqua&logoColor=#0018A8)",
     },
     {
@@ -1087,6 +1087,14 @@ export const data = {
     {
       label: "BABEL",
       url: "![Babel](https://img.shields.io/badge/Babel-F9DC3e?style=for-the-badge&logo=babel&logoColor=black)",
+    },
+    {
+      label: "BITWARDEN",
+      url: "![Bitwarden](https://img.shields.io/badge/bitwarden-%23175DDC.svg?style=for-the-badge&logo=bitwarden&logoColor=white)",
+    },
+    {
+      label: "CISCO",
+      url: "![Cisco](https://img.shields.io/badge/cisco-%23049fd9.svg?style=for-the-badge&logo=cisco&logoColor=black)",
     },
     {
       label: "CMAKE",
@@ -1101,20 +1109,68 @@ export const data = {
       url: "![Confluence](https://img.shields.io/badge/confluence-%23172BF4.svg?style=for-the-badge&logo=confluence&logoColor=white)",
     },
     {
+      label: "DOCKER",
+      url: "![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)",
+    },
+    {
       label: "ESLINT",
       url: "![ESLint](https://img.shields.io/badge/ESLint-4B3263?style=for-the-badge&logo=eslint&logoColor=white)",
+    },
+    {
+      label: "ELASTICSEARCH",
+      url: "![ElasticSearch](https://img.shields.io/badge/-ElasticSearch-005571?style=for-the-badge&logo=elasticsearch)",
     },
     {
       label: "GRADLE",
       url: "![Gradle](https://img.shields.io/badge/Gradle-02303A.svg?style=for-the-badge&logo=Gradle&logoColor=white)",
     },
     {
+      label: "GRAFANA",
+      url: "![Grafana](https://img.shields.io/badge/grafana-%23F46800.svg?style=for-the-badge&logo=grafana&logoColor=white)",
+    },
+    {
+      label: "HOME ASSISTANT",
+      url: "![Home Assistant](https://img.shields.io/badge/home%20assistant-%2341BDF5.svg?style=for-the-badge&logo=home-assistant&logoColor=white)",
+    },
+    {
+      label: "HOMEBRIDGE",
+      url: "![Homebridge](https://img.shields.io/badge/homebridge-%23491F59.svg?style=for-the-badge&logo=homebridge&logoColor=white)",
+    },
+    {
+      label: "JELLYFIN",
+      url: "![Jellyfin](https://img.shields.io/badge/jellyfin-%23000B25.svg?style=for-the-badge&logo=Jellyfin&logoColor=00A4DC)",
+    },
+    {
       label: "JIRA",
       url: "![Jira](https://img.shields.io/badge/jira-%230A0FFF.svg?style=for-the-badge&logo=jira&logoColor=white)",
     },
     {
+      label: "KUBERNETES",
+      url: "![Kubernetes](https://img.shields.io/badge/kubernetes-%23326ce5.svg?style=for-the-badge&logo=kubernetes&logoColor=white)",
+    },
+    {
+      label: "MOSQUITTO",
+      url: "![Mosquitto](https://img.shields.io/badge/mosquitto-%233C5280.svg?style=for-the-badge&logo=eclipsemosquitto&logoColor=white)",
+    },
+    {
       label: "NOTION",
       url: "![Notion](https://img.shields.io/badge/Notion-%23000000.svg?style=for-the-badge&logo=notion&logoColor=white)",
+    },
+    {
+      label: "OPENSEA",
+      url: "![OpenSea](https://img.shields.io/badge/OpenSea-%232081E2.svg?style=for-the-badge&logo=opensea&logoColor=white)",
+    },
+    {
+      label: "PACKER",
+      url: "![Packer](https://img.shields.io/badge/packer-%23E7EEF0.svg?style=for-the-badge&logo=packer&logoColor=%2302A8EF)",
+    },
+    {
+      label: "PI-HOLE",
+      url: "![Pi-Hole](https://img.shields.io/badge/pihole-%2396060C.svg?style=for-the-badge&logo=pi-hole&logoColor=white)",
+    },
+    {
+      label: "PLEX",
+      url: "![Plex](https://img.shields.io/badge/plex-%23E5A00D.svg?style=for-the-badge&logo=plex&logoColor=white)",
     },
     {
       label: "PORTFOLIO",
@@ -1125,28 +1181,76 @@ export const data = {
       url: "![Postman](https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white)",
     },
     {
+      label: "POWER BI",
+      url: "![Power Bi](https://img.shields.io/badge/power_bi-F2C811?style=for-the-badge&logo=powerbi&logoColor=black)",
+    },
+    {
       label: "PREZI",
       url: "![Prezi](https://img.shields.io/badge/Prezi-%23000000.svg?style=for-the-badge&logo=Prezi&logoColor=white)",
+    },
+    {
+      label: "PROMETHEUS",
+      url: "![Prometheus](https://img.shields.io/badge/Prometheus-E6522C?style=for-the-badge&logo=Prometheus&logoColor=white)",
     },
     {
       label: "RANCHER",
       url: "![Rancher](https://img.shields.io/badge/rancher-%230075A8.svg?style=for-the-badge&logo=rancher&logoColor=white)",
     },
     {
-      label: "RASPBERRYPI",
+      label: "RASPBERRY PI",
       url: "![Raspberry Pi](https://img.shields.io/badge/-RaspberryPi-C51A4A?style=for-the-badge&logo=Raspberry-Pi)",
+    },
+    {
+      label: "SONARLINT",
+      url: "![SonarLint](https://img.shields.io/badge/SonarLint-CB2029?style=for-the-badge&logo=SONARLINT&logoColor=white)",
+    },
+    {
+      label: "SONARQUBE",
+      url: "![SonarQube](https://img.shields.io/badge/SonarQube-black?style=for-the-badge&logo=sonarqube&logoColor=4E9BCD)",
+    },
+    {
+      label: "SPLUNK",
+      url: "![Splunk](https://img.shields.io/badge/splunk-%23000000.svg?style=for-the-badge&logo=splunk&logoColor=white)",
     },
     {
       label: "SWAGGER",
       url: "![Swagger](https://img.shields.io/badge/-Swagger-%23Clojure?style=for-the-badge&logo=swagger&logoColor=white)",
     },
     {
+      label: "TOR",
+      url: "![TOR](https://img.shields.io/badge/tor-%237E4798.svg?style=for-the-badge&logo=tor-project&logoColor=white)",
+    },
+    {
+      label: "TERRAFORM",
+      url: "![Terraform](https://img.shields.io/badge/terraform-%235835CC.svg?style=for-the-badge&logo=terraform&logoColor=white)",
+    },
+    {
       label: "TRELLO",
       url: "![Trello](https://img.shields.io/badge/Trello-%23026AA7.svg?style=for-the-badge&logo=Trello&logoColor=white)",
     },
     {
-      label: "WEBFLOW",
-      url: "![Webflow](https://img.shields.io/badge/Webflow-4353FF?style=for-the-badge&logo=webflow&logoColor=white)",
+      label: "UBER",
+      url: "![Uber](https://img.shields.io/badge/Uber-%23000000.svg?style=for-the-badge&logo=Uber&logoColor=white)",
+    },
+    {
+      label: "UBIQUITI",
+      url: "![Ubiquiti](https://img.shields.io/badge/ubiquiti-%230559C9.svg?style=for-the-badge&logo=ubiquiti&logoColor=white)",
+    },
+    {
+      label: "VAGRANT",
+      url: "![Vagrant](https://img.shields.io/badge/vagrant-%231563FF.svg?style=for-the-badge&logo=vagrant&logoColor=white)",
+    },
+    {
+      label: "WIREGUARD",
+      url: "![Wireguard](https://img.shields.io/badge/wireguard-%2388171A.svg?style=for-the-badge&logo=wireguard&logoColor=white)",
+    },
+    {
+      label: "XFCE",
+      url: "![XFCE](https://img.shields.io/badge/XFCE-%232284F2.svg?style=for-the-badge&logo=xfce&logoColor=white)",
+    },
+    {
+      label: "ZIGBEE",
+      url: "![Zigbee](https://img.shields.io/badge/zigbee-%23EB0443.svg?style=for-the-badge&logo=zigbee&logoColor=white)",
     },
   ],
 };
