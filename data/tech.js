c2eca11bfb29de1390659f943b96fa25197ceee3
Laurent Décamps <decamps.laurent@gmail.com>
2023-12-07 23:47:02 +0100

    fix: update tech.js for csharp logo to appear
    C# logo is spelled csharp not c-sharp

--- data/tech.js
+++ data/tech.js
@@ -10,7 +10,7 @@ export const data = {
     },
     {
       label: "C#",
-      url: "![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white)",
+      url: "![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white)",
     },
     {
       label: "C++",
