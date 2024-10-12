10f56e13c684aa743288710e43254b566c525c95
Zeeshan Ahmad <itexpert120@outlook.com>
2024-10-12 13:32:47 +0500

    Address npm install issues

--- components/elements/AnimatedText.js
+++ components/elements/AnimatedText.js
@@ -1,21 +1,11 @@
 import React from "react";
-import TextLoop from "react-text-loop/lib/components/TextLoop";
 
 export default function AnimatedText() {
   return (
     <div className="text-white hidden md:block sm:text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl font-medium overflow-y-hidden">
       Create a {/* <TextLoop> */}
       <span className="text-white ">Near Social</span>
-      {/* <span className="text-cyan-200 underline underline-offset-4 decoration-dashed">
-          Modern
-        </span>
-        <span className="text-amber-200 underline underline-offset-4 decoration-dashed">
-          Interactive
-        </span>
-        <span className="text-green-200 underline underline-offset-4 decoration-dashed">
-          Perfect
-        </span> */}
-      {/* </TextLoop> */} Profile
+      Profile
     </div>
   );
 }
